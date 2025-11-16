81a7a7759ef5f272bf661a9e299a112410f53731
faAanya <pkirpichenok@gmail.com>
2025-11-16 14:14:18 +0300

    feat: create sign up form

---
+++ frontend/auth/src/app/user/registration/registration.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-registration',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './registration.html',
+  styles: ``,
+})
+export class Registration {
+   form: any;
+  constructor(public formBuilder: FormBuilder){
+    this.form = this.formBuilder.group({
+    fullName:[''],
+    email:[''],
+    password:[''],
+    confirmPassword:['']
+  })
+  }
+
+  onSubmit(){
+    console.log(this.form.value);
+    
+  }
+}
